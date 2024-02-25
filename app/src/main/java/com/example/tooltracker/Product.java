25a7a3fabf0d71697813ad72bb885f77ff972e1d
MarielaSolanoG <128342193+MarielaSolanoG@users.noreply.github.com>
2024-02-24 23:32:37 -0600

    Agregada funcionalidad registrar producto

---
+++ app/src/main/java/com/example/tooltracker/Product.java
@@ -0,0 +1,69 @@
+package com.example.tooltracker;
+
+public class Product {
+    private int productId; // ID del producto
+    private String name; // Nombre del producto
+    private int quantity; // Cantidad disponible
+    private float priceCost; // Precio de costo
+    private float priceSale; // Precio de venta
+    private int quantitySold; // Cantidad vendida
+
+    // Constructor
+    public Product(int productId, String name, int quantity, float priceCost, float priceSale, int quantitySold) {
+        this.productId = productId;
+        this.name = name;
+        this.quantity = quantity;
+        this.priceCost = priceCost;
+        this.priceSale = priceSale;
+        this.quantitySold = quantitySold;
+    }
+
+    // Getters y setters
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public float getPriceCost() {
+        return priceCost;
+    }
+
+    public void setPriceCost(float priceCost) {
+        this.priceCost = priceCost;
+    }
+
+    public float getPriceSale() {
+        return priceSale;
+    }
+
+    public void setPriceSale(float priceSale) {
+        this.priceSale = priceSale;
+    }
+
+    public int getQuantitySold() {
+        return quantitySold;
+    }
+
+    public void setQuantitySold(int quantitySold) {
+        this.quantitySold = quantitySold;
+    }
+}
--- app/src/main/java/com/example/tooltracker/RegisterProductActivity.java
+++ app/src/main/java/com/example/tooltracker/RegisterProductActivity.java
@@ -4,8 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
-
-import com.example.tooltracker.R;
+import android.widget.EditText;
+import java.util.List;
 
 public class RegisterProductActivity extends Activity {
     @Override
@@ -14,8 +14,50 @@ public class RegisterProductActivity extends Activity {
         setContentView(R.layout.activity_register_product);
     }
     public void onAddProductButtonClick(View view) {
-        // Lógica para agregar un producto
-        Toast.makeText(this, "Agregando producto...", Toast.LENGTH_SHORT).show();
+        // Obtener referencias a los campos de texto
+        EditText idEditText = findViewById(R.id.editTextProductId);
+        EditText nameEditText = findViewById(R.id.editTextProductName);
+        EditText priceCostEditText = findViewById(R.id.editTextProductPriceCost);
+        EditText priceSaleEditText = findViewById(R.id.editTextProductPriceSale);
+        EditText quantityEditText = findViewById(R.id.editTextProductQuantity);
+        EditText quantitySoldEditText = findViewById(R.id.editTextProductQuantitySold);
+
+        // Obtener texto ingresado por el usuario
+        String productIdText = idEditText.getText().toString();
+        String name = nameEditText.getText().toString();
+        int quantity = Integer.parseInt(quantityEditText.getText().toString());
+        int quantitySold = Integer.parseInt(quantitySoldEditText.getText().toString());
+        float priceCost = Float.parseFloat(priceCostEditText.getText().toString());
+        float priceSale = Float.parseFloat(priceSaleEditText.getText().toString());
+
+        // Verificar si el ID del producto es válido
+        if (productIdText.isEmpty()) {
+            // Mostrar un mensaje de error si el campo de ID del producto está vacío
+            Toast.makeText(this, "Ingrese el ID del producto", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // Convertir el ID del producto a un número entero
+        int productId = Integer.parseInt(productIdText);
+        // Crear un nuevo producto
+        Product newProduct = new Product(productId, name, quantity, priceCost, priceSale, quantitySold);
+        System.out.println(newProduct);
+        ProductManager productManager = MainActivity.productManager;
+        productManager.addProduct(newProduct);
+        List<Product> productList = productManager.getProductList();
+        for (Product product : productList) {
+            System.out.println("Product ID: " + product.getProductId());
+            System.out.println("Name: " + product.getName());
+            System.out.println("Quantity: " + product.getQuantity());
+        }
+        Toast.makeText(this, "Producto agregado...", Toast.LENGTH_SHORT).show();
+
+        idEditText.setText("");
+        nameEditText.setText("");
+        priceCostEditText.setText("");
+        priceSaleEditText.setText("");
+        quantityEditText.setText("");
+        quantitySoldEditText.setText("");
+
     }
 
     public void onExitButtonClick(View view) {
