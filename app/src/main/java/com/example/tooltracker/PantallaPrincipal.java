4b6029e99a7b8ca2c30b77752ee3f1a4dc42afa4
MarielaSolanoG <128342193+MarielaSolanoG@users.noreply.github.com>
2024-03-21 00:12:04 -0600

    --implementado 100% menú hamburguesa :)

--- app/src/main/java/com/example/tooltracker/PantallaPrincipal.java
+++ app/src/main/java/com/example/tooltracker/PantallaPrincipal.java
@@ -1,13 +1,21 @@
 package com.example.tooltracker;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.android.material.navigation.NavigationView;
+import com.example.tooltracker.R;
 
-public class PantallaPrincipal extends AppCompatActivity {
+public class PantallaPrincipal extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
@@ -22,19 +30,45 @@ public class PantallaPrincipal extends AppCompatActivity {
         navigationView = findViewById(R.id.nav_view);
         toolbar = findViewById(R.id.toolbar);
 
-        // Establecer la Toolbar como ActionBar
         setSupportActionBar(toolbar);
 
-        // Crear el ActionBarDrawerToggle y vincularlo con el DrawerLayout
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawerLayout, toolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close
-        );
-
-        // Agregar el ActionBarDrawerToggle como listener al DrawerLayout
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
-
-        // Sincronizar el estado del ActionBarDrawerToggle con el DrawerLayout
         toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            // Manejar el clic en el elemento del menú
+            if (item.getItemId() == R.id.nav_home) {
+                Intent intent = new Intent(this, PantallaPrincipal.class);
+                startActivity(intent);
+            } else if (item.getItemId() == R.id.nav_register) {
+                Intent intent = new Intent(this, RegisterProductActivity.class);
+                startActivity(intent);
+
+            } else if (item.getItemId() == R.id.nav_consult) {
+                Intent intent = new Intent(this, ConsultProductActivity.class);
+                startActivity(intent);
+            } else if (item.getItemId() == R.id.nav_delete) {
+                Intent intent = new Intent(this, DeleteProductActivity.class);
+                startActivity(intent);
+            } else if (item.getItemId() == R.id.nav_modify) {
+                Intent intent = new Intent(this, ModifyProductActivity.class);
+                startActivity(intent);
+            } else if (item.getItemId() == R.id.nav_map) {
+                //Intent intent = new Intent(this, GoogleMapActivity.class);
+                //startActivity(intent);
+    }
+
+        // Cerrar el DrawerLayout después de manejar el clic en el elemento del menú
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
+
